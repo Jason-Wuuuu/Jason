99d73890b9602ffb579e745c1187a45fde3ac289
Chia-Hsiang Wu <cwu34@stevens.edu>
2024-03-06 15:17:12 -0500

    nlp projects added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -29,6 +29,30 @@ const projects = [
     ],
   },
   {
+    title: "Word2Vec Skip-Gram model",
+    year: "2024",
+    course: "NLP Course",
+    category: "ML/DL",
+    description:
+      "Word2Vec Skip-Gram model using TensorFlow to analyze and generate word embeddings from the IMDB movie review dataset, enhancing the understanding of semantic word relationships.",
+    tech_stack: "Python, TensorFlow, NumPy, scikit-learn, Matplotlib",
+    githubUrl: "",
+    demoUrl: "",
+    screenshots: ["word2vec/word_vec.png"],
+  },
+  {
+    title: "Multi-Layer Perceptron (MLP) model",
+    year: "2024",
+    course: "NLP Course",
+    category: "ML/DL",
+    description:
+      "Multi-Layer Perceptron (MLP) model from scratch in Python, applying concepts of neural networks to perform multi-class text classification on a large dataset, demonstrating deep understanding of foundational machine learning algorithms.",
+    tech_stack: "Python, TensorFlow, NumPy, scikit-learn, Matplotlib",
+    githubUrl: "",
+    demoUrl: "",
+    screenshots: ["mlp/loss.png", "mlp/score.png"],
+  },
+  {
     title: "Visual Board",
     year: "2024",
     course: "Personal",
