aa3cf059cfcec8440166f3ce0b94bea5c77e07ce
Chia-Hsiang Wu <cwu34@stevens.edu>
2024-07-14 18:01:56 -0400

    margin

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
             variant="h3"
             align="center"
             fontWeight="bold"
-            mb={5}
+            mb={8}
             sx={{ display: { xs: "none", sm: "block" } }}
           >
             👋 Jason's Portfolio
